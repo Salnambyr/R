eee2531ae380eef2a42053cbc0051aa755917edb
Salnambyr <snambiyar@dons.usfca.edu>
2020-02-11 21:12:07 -0800

    second practice file

---
+++ .R
@@ -0,0 +1,31 @@
+just_add <- function(a,b){ a+b}
+}
+
+  
+  if(condition) {do something}
+    else { do something}
+
+
+a<-9
+if (a<10) 
+{ print ("It is less than ten")} else { print("its more than or equal to ten")}
+
+  
+  
+  
+  
+tell_odd<- function(x){ if (x%%2!=0){print 'it is an odd number') else {print('its an even number')}}
+    
+tell_odd(5)
+tell_odd(6)
+
+a <- 10
+i = 0
+
+while(i > 1) {
+  print ( paste("loop", i))
+  i = i + 1
+}
+}
+    
+    
